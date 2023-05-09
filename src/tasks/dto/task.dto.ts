2189dc5c3b4e2d08ca0d2ed4e69198a17f1957a1
LuiiVasquez <luivasque95@gmail.com>
2023-05-08 23:22:10 -0600

    body validations added

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,15 +1,37 @@
 // * archivo para la validacion del body para el controller
 
 import { TaskStatus } from '../task.entity';
+import {
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsOptional,
+  IsIn,
+} from 'class-validator';
 
 export class CreateTaskDto {
+  @IsString() // -> decorador para validar el tipo de dato
+  @IsNotEmpty()
+  @MinLength(3)
   title: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   description: string;
 }
 
 // -> el ? es para hacerle saber a TS que puede llegar el dato o no
 export class UpdateTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.PENDING]) // -> para validar un enum, puede venir del entity
   status?: TaskStatus;
 }
